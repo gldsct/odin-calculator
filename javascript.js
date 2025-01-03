1ed5fe1c9da9b65e564ccc0dc371741d11bc287a
gldsct <188839581+gldsct@users.noreply.github.com>
2025-01-03 23:11:57 +0545

    Create functions for calculation operations

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+function add(firstOperand, secondOperand) {
+    return Number(firstOperand) + Number(secondOperand);
+}
+
+function subtract(firstOperand, secondOperand) {
+    return Number(firstOperand) - Number(secondOperand);
+}
+
+function multiply(firstOperand, secondOperand) {
+    return Number(firstOperand) * Number(secondOperand);
+}
+
+function divide(firstOperand, secondOperand) {
+    return Number(firstOperand) / Number(secondOperand);
+}
+
 let calculationInput = document.querySelector("#calculation-input");
 let calculationInputButton = document.querySelector(".calculation-input-button");
 
